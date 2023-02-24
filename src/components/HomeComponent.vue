aecd773e2c64db88411231fab900fba6ca9b256c
Abir Bhattacharya <70687014+abirbhattacharya82@users.noreply.github.com>
2023-02-24 15:19:51 +0000

    Home

---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="Title">
+        Quiz App
+    </div>
+    <div class="Subtitle" v-if="showC1">
+        Click on the button below to start the Quiz
+    </div>
+    <button @click="start()" v-if="showC1">Start Quiz</button>
+</template>
+
+<script>
+export default {
+  data() 
+  {
+    return {
+      showC1: true
+    };
+  },
+  methods:
+  {
+    start()
+    {
+        this.show=false
+    }
+  }
+}
+</script>
