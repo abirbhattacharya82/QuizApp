1af2d8800ecf7d1853c69eaf434f14232ae2d6ef
Abir Bhattacharya <70687014+abirbhattacharya82@users.noreply.github.com>
2023-02-25 03:16:52 +0000

    Instruction Components

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,22 +1,53 @@
 <template>
   <TitleComponent />
-  <FunctionComponent />
-  <InstructionComponent />
+  <!-- <FunctionComponent /> -->
+  <InstructionComponent v-if="show"/>
   <div class="StartArea">
-    <button>Start</button>
+    <button @click="startQuiz()" id="startButton">Start</button>
   </div>
 </template>
 
 <script>
 import TitleComponent from './TitleComponent.vue';
-import FunctionComponent  from './FunctionComponent.vue';
+// import FunctionComponent  from './FunctionComponent.vue';
 import InstructionComponent from './InstructionComponent.vue';
 export default{
+  data(){
+    return{
+      show:true
+    }
+  },
   name:'HomeComponent',
   components:{
     TitleComponent,
-    FunctionComponent,
+    // FunctionComponent,
     InstructionComponent
-}
+  },
+  methods:{
+    startQuiz(){
+      this.show=false;
+      document.getElementById('startButton').style.display="none";
+    }
+  }
 }
 </script>
+
+<style>
+.StartArea
+{
+  display: flex;
+  justify-content: center;
+}
+.StartArea button
+{
+  background-color: rgb(64, 255, 47);
+  color: white;
+  border: none;
+  padding: 1%;
+  font-size: 150%;
+}
+.StartArea button:hover
+{
+  background-color: rgb(39, 150, 29);
+}
+</style>
--- src/components/InstructionComponent.vue
+++ src/components/InstructionComponent.vue
@@ -1,3 +1,24 @@
 <template>
-    Instructions
+    <div class="Instructions">
+        <ul>
+            <li>Click on the Start button to start the Quiz</li>
+            <li>Once the Game is started click on the correct answer and click on the submit button</li>
+            <li>If its a right answer you will get to know it</li>
+            <li>If its a wrong answer you will get to know it as well</li>
+            <li>For every correct answer you get a +1 point</li>
+            <li>For every wrong answer the Computer gets a +1 point</li>
+        </ul>
+    </div>
 </template>
+
+<style>
+.Instructions
+{
+    display: flex;
+    justify-content: center;
+}
+.Instructions ul li
+{
+    font-size: 150%;
+}
+</style>
